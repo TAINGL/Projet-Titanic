66f665c3ea53c98a278f8e0ec5fc63e11bce9ab4
Laura TAING <48871171+TAINGL@users.noreply.github.com>
2020-05-10 20:54:16 +0200

    Change ML

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.compose import ColumnTransformer, make_column_transformer, make_column_selector
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.metrics import accuracy_score, classification_report
 
 from sklearn.svm import LinearSVC, SVC
 from sklearn.linear_model import LinearRegression, LogisticRegression, SGDClassifier
@@ -46,22 +49,22 @@ In this challenge, we ask you to build a predictive model that answers the quest
 # EDA
 DATA_URL = "train.csv"
 
-# Preprocessing data
 def age_missing_replace(means, dframe, title_list):
     for title in title_list:
         temp = dframe['Title'] == title 
         dframe.loc[temp, 'Age'] = dframe.loc[temp, 'Age'].fillna(means[title]) 
 
+
 # To Improve speed and cache data
 @st.cache(allow_output_mutation=True)
 def load_data():
     data = pd.read_csv(os.path.join(DATA_URL))
-    data = data.drop(['Ticket','PassengerId'],axis=1)
     data['Title'] = data.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
     data['Title'] = data['Title'].replace(['Dr', 'Rev', 'Col','Major', 'Countess', 'Sir', 'Jonkheer', 'Lady', 'Capt', 'Don'], 'Others')
     data['Title'] = data['Title'].replace('Ms', 'Miss')
     data['Title'] = data['Title'].replace('Mme', 'Mrs')
     data['Title'] = data['Title'].replace('Mlle', 'Miss')
+
     data['Embarked'] = data['Embarked'].fillna(data['Embarked'].mode()[0])
     data['Fare'] = data['Fare'].round(0) 
     
@@ -78,17 +81,18 @@ def load_data():
     data['Embarked'] = lb_make.fit_transform(data['Embarked']) # 0:C, 1:Q , 2:S
     data['Title'] = lb_make.fit_transform(data['Title']) # 0:Master, 1:Miss, 2:Mr , 3:Mrs, 4:Others
     data['Sex'] = lb_make.fit_transform(data['Sex']) # 0: M, 1:F
-    data = data.drop(['Name', 'Cabin'],axis=1)
-    #data['Cabin'] = data['Cabin'].fillna('NO CABIN', inplace = True) # if we want to keep this columns
+    data = data.drop(['Ticket','PassengerId','Cabin','Name'],axis=1)
+
 
     return data
 
 # Control load data
 data_load_state = st.text('Loading data...')
-training_set = load_data()
+data = load_data()
 data_load_state.text('Loading data...done!')
 
-st.markdown("## 1. Description du dataset")
+################################################################
+st.markdown("## 1. Dataset description")
 # Show Dataset
 if st.checkbox("Preview DataFrame"):
     data = load_data()
@@ -125,6 +129,7 @@ if st.checkbox("Show Summary of Dataset"):
     data = load_data()
     st.write(data.describe())
 
+################################################################
 st.markdown("## 2. Description of variables")
 """
 **Variable Notes**
@@ -172,15 +177,10 @@ else:
     st.write("Select A Column")
 
 
-st.markdown("## 3. Graphique")
+################################################################
+st.markdown("## 3. Graphics")
 
 COLUMN = 'Sex'
-
-@st.cache
-def load_data(nrows):
-    data = load_data()
-    data[COLUMN] = pd.DataFrame(data[COLUMN])
-    return data
 
 st.subheader('Probability of survival (Age and Sex)')
 # Choose between woman and man
@@ -199,109 +199,124 @@ ax.legend()
 ax.set_title(genre)
 st.pyplot()
 
+################################################################
 st.markdown("## 4. Resultat of Prediction Titanic Games")
-st.subheader("The learning model used is Random Tree Forest")
+st.subheader("Prediction")
 
 # Prediction Titanic Games
-# Give the input data
-st.sidebar.subheader("✔️ Prediction Titanic Games")
-
-st.sidebar.title("Sexe")
-Sexe = st.sidebar.selectbox("Choose your genre: ", ('M', 'F')) # 0: M, 1:F
-
-st.sidebar.title("Class")
-Class = st.sidebar.selectbox("Choose your class: ", np.arange(1,4,1))
-
-st.sidebar.title("Age")
-min_age = min(training_set['Age'])
-max_age = max(training_set['Age'])
-Age = st.sidebar.slider("Choose your age: ", min_value=round(min_age), max_value=round(max_age), value=20, step=1)
-
-st.sidebar.title("Ticket Price")
-min_ticket = min(training_set['Fare'])
-max_ticket = max(training_set['Fare'])
-Fare = st.sidebar.slider("Choose your ticket price: ", min_value=round(min_ticket), max_value=round(max_ticket), value=100, step=1)
-
-st.sidebar.title("Family Size")
-training_set["FamilySize"] = training_set["SibSp"] + training_set["Parch"] + 1
-min_fsize = min(training_set['FamilySize'])
-max_fsize = max(training_set['FamilySize'])
-FamilySize = st.sidebar.slider("What is your family size: ", min_value=round(min_fsize), max_value=round(max_fsize), value=1, step=1)
-
-st.sidebar.title("Embarked")
-Embarked = st.sidebar.selectbox("Choose your embarked: ", ('Cherbourg', 'Queenstown','Southampton')) # 0:C, 1:Q , 2:S
-titanic_gate = {'Cherbourg':0, 'Queenstown':1, 'Southampton':2}
-
-
-# Application with model
-X = training_set.drop(['Survived','Title','SibSp', 'Parch'],axis=1)
-y = training_set["Survived"]
 
 # Choose your ML model
-#model_choice = ['LinearSVC', 'SVC',
-#          'LinearRegression', 'LogisticRegression', 'SGDClassifier', 
-#          'GaussianNB', 'MultinomialNB', 'ComplementNB', 'BernoulliNB',
-#          'KNeighborsClassifier', 'KNeighborsRegressor',
-#          'DecisionTreeClassifier', 'RandomForestClassifier']
+def classifiers(classifier):
+    classifiers = {
+        'Random Forest': RandomForestClassifier(),
+        'Regression logistique': LogisticRegression(),
+        'KNN': KNeighborsClassifier(),
+        'LinearSVC': LinearSVC(), 
+        'SVC': SVC(),
+        'DecisionTreeClassifier': DecisionTreeClassifier(),
+        'LinearRegression': LinearRegression(), 
+        'LogisticRegression': LogisticRegression(),
+        'GaussianNB': GaussianNB(), 
+        'MultinomialNB': MultinomialNB(), 
+        'ComplementNB': ComplementNB(), 
+        'BernoulliNB': BernoulliNB(),      
+    }
 
-#st.sidebar.title("ML Model")
-#Model = st.sidebar.selectbox("Choose your model: ", (model_choice)) 
+    return classifiers[classifier]
 
-#model_list = (LinearSVC(), SVC(), 
-#          LinearRegression(), LogisticRegression(), SGDClassifier(), 
-#          GaussianNB(), MultinomialNB(), ComplementNB(), BernoulliNB(),
-#          KNeighborsClassifier(), KNeighborsRegressor(),
-#          DecisionTreeClassifier(), RandomForestClassifier())
+# Give the input data, add a selector for the app mode on the sidebar.
+    
+def input_data():
+    st.sidebar.subheader("✔️ Prediction Titanic Games")
 
-#def run_model(Model):
-#    if Model == 'LinearSVC':
-#        thismodel = model_list[0]
-#    elif Model == 'SVC':
-#        thismodel = model_list[1]
-#    elif Model == 'LinearRegression':
-#        thismodel = model_list[2]
-#    elif Model == 'LogisticRegression':
-#        thismodel = model_list[3]
-#    elif Model == 'SGDClassifier':
-#        thismodel = model_list[4]
-#    elif Model == 'GaussianNB':
-#        thismodel = model_list[5]
-#    elif Model == 'MultinomialNB':
-#        thismodel = model_list[6]
-#    elif Model == 'ComplementNB':
-#        thismodel = model_list[7]
-#    elif Model == 'BernoulliNB':
-#        thismodel = model_list[8]
-#    elif Model == 'KNeighborsClassifier':
-#        thismodel = model_list[9]
-#    elif Model == 'KNeighborsRegressor':
-#        thismodel = model_list[10]
-#    elif Model == 'DecisionTreeClassifier':
-#        thismodel = model_list[11]
-#    else:
-#        thismodel = model_list[12]
-   
+    st.sidebar.title("Sexe")
+    Sexe = st.sidebar.selectbox("Choose your genre: ", ('M', 'F')) # 0: M, 1:F
+
+    st.sidebar.title("Class")
+    Class = st.sidebar.selectbox("Choose your class: ", data['Pclass'].unique())
+
+    st.sidebar.title("Age")
+    min_age = min(data['Age'])
+    max_age = max(data['Age'])
+    Age = st.sidebar.slider("Choose your age: ", min_value=round(min_age), max_value=round(max_age), value=20, step=1)
+
+    st.sidebar.title("Ticket Price")
+    min_ticket = min(data['Fare'])
+    max_ticket = max(data['Fare'])
+    Fare = st.sidebar.slider("Choose your ticket price: ", min_value=round(min_ticket), max_value=round(max_ticket), value=100, step=1)
+
+    st.sidebar.title("Family Size")
+    data["FamilySize"] = data["SibSp"] + data["Parch"] + 1
+    min_fsize = min(data['FamilySize'])
+    max_fsize = max(data['FamilySize'])
+    FamilySize = st.sidebar.slider("What is your family size: ", min_value=round(min_fsize), max_value=round(max_fsize), value=1, step=1)
+
+    st.sidebar.title("Embarked")
+    Embarked = st.sidebar.selectbox("Choose your embarked: ", ('Cherbourg', 'Queenstown','Southampton')) # 0:C, 1:Q , 2:S
+    titanic_gate = {'Cherbourg':0, 'Queenstown':1, 'Southampton':2}
+
+    st.sidebar.title("ML Model")
+    Model = st.sidebar.selectbox("Modele", ('Random Forest', 'Regression logistique', 'KNN', 'LinearSVC', 'SVC', 
+    'LinearRegression', 'LogisticRegression', 'GaussianNB', 'MultinomialNB', 'ComplementNB', 'BernoulliNB'))
+
+    if Sexe == "M":
+        Sexe_id = 0
+    else:
+        Sexe_id = 1
 
 
-model = RandomForestClassifier() #model = thismodel
-model.fit(X, y)
+    #dataframe user
+    input_user = np.array([Sexe_id, Class, Age, FamilySize, Fare, titanic_gate[Embarked]]).reshape(1,-1)
+    df_user = pd.DataFrame(input_user, columns = ['Sex','Class','Age','FamilySize','Fare','Embarked']) 
 
-if Sexe == "M":
-    Sexe_id = 0
-else:
-    Sexe_id = 1
+    #classifier
+    classifier=classifiers(Model)
 
-titanic_id = [[Sexe_id, Class, Age, FamilySize, Fare, titanic_gate[Embarked]]]
+    return df_user, classifier
 
-# Prédiction de survie de l'id
-survived = model.predict(titanic_id)
-# probabilité de survie calculée par le modèle
-proba = model.predict_proba(titanic_id)
+# Application with model
+def modele():
 
+    df_user, classifier=input_data()
+
+    # on crée un pipeline de traitement intégrant la préparation
+    pipeline_ml = classifier
+
+    # on sépare la cible du reste des données
+    data = load_data()
+    X = data.drop(['Survived','Title','SibSp', 'Parch'],axis=1)
+    y = data['Survived']
+
+    # on construit les échantillons d'apprentissage et de validation
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    # on ajuste le modèle en utilisant les données d'apprentissage
+    # le modèle comporte la préparation et le modèle logistique
+    pipeline_ml.fit(X_train, y_train)
+    y_pred=pipeline_ml.predict(X_test)
+    
+	# metrics
+    score =accuracy_score(y_test, y_pred)
+    report= confusion_matrix(y_test, y_pred)
+
+    # Probabilité de survie calculée par le modèle
+    user_prediction = pipeline_ml.predict_proba(df_user)
+
+    return user_prediction, score, report 
+
+# Prediction
+user_prediction, score, report = modele()
+
+st.subheader('Model Metrics')
+st.text("Accuracy")
+st.write(round(score,2),"%")
+st.text("Report: ")
+st.write(report)  
+    
 if survived[0] == 1:
-	st.success("The person has  {}% of being alive.".format(round(proba[0,1]*100,2)))
+	st.success("The person has  {}% of being alive.".format(round(user_prediction[0,1]*100,2)))
 else:
-	st.error("The person has  {}% of being dead.".format(round(proba[0,0]*100,2)))
+	st.error("The person has  {}% of being dead.".format(round(user_prediction[0,0]*100,2)))
 
 # About
 
